48fd1aa4786cee4de85e7787fa0219ff1b96332f
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2022-11-25 12:24:18 +0100

    cleaning repo

--- multibind/tl/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def supertest(m):
-    count = 0
-    for i in range(m.shape[0]):
-        for j in range(m.shape[0]):
-            count += m[i, j]
-    return np.mean(count)
