089d1d7d27b15771f69d11968822cb285106a476
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2022-12-01 12:43:19 +0100

    gene_names_test_fix

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,7 +6,7 @@ import torch.utils.data as tdata
 import os
 
 def test_configtest_exists():
-    output_state = os.system('snakemake --configfile config_test.yaml -c1 --dry-run ')
+    output_state = os.system('snakemake --configfile config_test.yaml -c1 --dry-run')
     assert output_state == 0
 
 def test_configtest_executes():
