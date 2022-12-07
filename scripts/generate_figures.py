0fc4c3cb0b5e948f149fba4dc2ef3ed56a4204da
ege-erdogan <ege@erdogan.dev>
2022-12-07 15:52:30 +0100

    add r2 plot per library and exception handling fix around tf.name issue

--- scripts/generate_figures.py
+++ scripts/generate_figures.py
@@ -27,16 +27,31 @@ if __name__ == '__main__':
         metrics = pd.read_csv(f)
         results[tf_name] = metrics
 
-    # 1. one file for each tf, plot r2 values wrt all hyperparams 
+    r2_y_vars = ['r2_counts', 'r2_foldchange', 'r2_enr', 'r2_fc', 'pearson_foldchange']
+    params_x = ['n_epochs', 'n_kernels', 'learning_rate', 'batch_size']
+
+    # PLOT #1. one file for each tf. r2 values wrt all hyperparams 
     for tf in tf_names:
         plot_name = f'{args.output}/{tf}_full.png'
-        g = sns.PairGrid(results[tf], y_vars=['r2_counts', 'r2_foldchange', 'r2_enr', 'r2_fc', 'pearson_foldchange'],
-                        x_vars=['n_epochs', 'n_kernels', 'learning_rate', 'batch_size'],
+        g = sns.PairGrid(results[tf], 
+                        y_vars=r2_y_vars, x_vars=params_x,
                         despine=True)
         g.map(sns.boxplot)
         g.savefig(plot_name)
         print(f'Saved {plot_name}.')
 
+
+    # PLOT #2. single file. r2 values for libraries
+    merged_results = pd.concat(results.values())
+    plot_name = f'{args.output}/library_r2.png'
+
+    g = sns.PairGrid(merged_results, 
+                    y_vars=r2_y_vars, x_vars=['library'], 
+                    despine=True, aspect=len(merged_results.groupby('library'))/2)
+    plt.xticks(rotation=90)
+    g.map(sns.boxplot)
+    g.savefig(plot_name)
+    print(f'Saved {plot_name}.')
     
     
 
